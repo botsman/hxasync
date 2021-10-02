b396ef3e4dfeda92555e293e2670f8506f2ed81a
Pavel <11509664+botsman@users.noreply.github.com>
2021-10-02 18:19:16 +0300

    fix parenthesis handling

--- src/hxasync/AsyncMacroUtils.hx
+++ src/hxasync/AsyncMacroUtils.hx
@@ -13,6 +13,16 @@ class AsyncMacroUtils {
     #end
   }
 
+  public static extern inline function awaitWithParenthesis<T>(arg: Awaitable<T>): T {
+    #if js
+    return std.js.Syntax.code("(await {0})", arg);
+    #elseif python
+    return std.python.Syntax.code("(await {0})", arg);
+    #else
+    return cast arg;
+    #end
+  }
+
   #if macro
   public static inline function count(text: String, char: String): Int {
     var counter = 0;
